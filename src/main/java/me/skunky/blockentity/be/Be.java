910bbc7372372f7176c9b6c9beca51ba3643892a
BuildTools <unconfigured@null.spigotmc.org>
2022-05-01 00:24:11 +0200

    .

--- src/main/java/me/skunky/blockentity/be/Be.java
+++ src/main/java/me/skunky/blockentity/be/Be.java
@@ -2,7 +2,6 @@ package me.skunky.blockentity.be;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
